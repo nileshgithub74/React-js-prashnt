87b865f279248dcc64b293497cb00b96ed1e8cb0
Nilesh kumar <kumarnilesh843127@gmail.com>
2024-09-22 01:41:16 +0530

    react js

--- react_course/src/App.jsx
+++ react_course/src/App.jsx
@@ -1,12 +1,21 @@
 
-function App() {
- 
-
-  return (
+import Nilesh from './Nilesh'
+function App(){
+  return(
     <>
-     <h1>this is nilesh kumar !!</h1>
+
+    <h1>React Course : By Prashant Sir </h1>
+    <h2>
+      welcome to this course !!
+    </h2>
+
+      <Nilesh></Nilesh>
+      <Nilesh></Nilesh>
+    
+
     </>
   )
+
 }
 
-export default App
+export default App;
---
+++ react_course/src/Nilesh.jsx
@@ -0,0 +1,14 @@
+
+
+function Nilesh(){
+
+
+  let myName = 'Neelesh'
+  return(
+    <>
+    <h2>heyy this is {myName} kumar</h2>
+    </>
+  )
+}
+
+export default Nilesh
